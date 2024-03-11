d694461027223017865b9db78c7703fce23e1d66
Roman Makhnev <romanmahnef777@gmail.com>
2024-03-11 20:18:14 +0400

    Complete 21.1

--- mymarket/models.py
+++ mymarket/models.py
@@ -4,3 +4,12 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    content = models.TextField()
+    preview_image = models.ImageField(upload_to='blog_images/')
+    created_date = models.DateTimeField(auto_now_add=True)
+    is_published = models.BooleanField(default=False)
+    views_count = models.IntegerField(default=0)
--- mymarket/views.py
+++ mymarket/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from .models import Product
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, TemplateView
+from django.urls import reverse
+from django.utils.text import slugify
+from .models import BlogPost
 
 def product_detail(request, pk):
     product = Product.objects.get(pk=pk)
@@ -13,3 +17,45 @@ def index(request):
     context = {'products': products}
 
     return render(request, 'index.html', context)
+
+
+class ContactView(TemplateView):
+    template_name = 'contacts.html'
+
+class BlogPostListView(ListView):
+    model = BlogPost
+    template_name = 'blogpost_list.html'
+
+class BlogPostDetailView(DetailView):
+    model = BlogPost
+    template_name = 'blogpost_detail.html'
+    queryset = BlogPost.objects.filter(is_published=True)
+
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)
+        obj.views_count += 1
+        obj.save()
+        return obj
+
+class BlogPostCreateView(CreateView):
+    model = BlogPost
+    template_name = 'blogpost_form.html'
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        new_blog = form.save(commit=False)
+        new_blog.slug = slugify(new_blog.title)
+        new_blog.save()
+        return super().form_valid(form)
+
+class BlogPostUpdateView(UpdateView):
+    model = BlogPost
+    template_name = 'blogpost_form.html'
+    fields = ['title', 'content']
+
+    def get_success_url(self):
+        return reverse('blogpost_detail', args=(self.object.pk,))
+
+class BlogPostDeleteView(DeleteView):
+    model = BlogPost
+    template_name = 'blogpost_confirm_delete.html'
