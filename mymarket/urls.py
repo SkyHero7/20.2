daf27e3be856de389dbddb37dedeca3d1661e8b1
Roman Makhnev <romanmahnef777@gmail.com>
2024-03-26 18:55:46 +0400

    Complete 22.1 with fix

--- mymarket/urls.py
+++ mymarket/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 from .views import ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView, ProductDeleteView
+from .views import VersionDetailView
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='index'),
@@ -9,4 +10,5 @@ urlpatterns = [
     path('product/<int:pk>/update/', views.product_update, name='product_update'),
     path('product/<int:pk>/delete/', views.product_delete, name='product_delete'),
     path('list/', views.product_list, name='product_list'),
+    path('product/version/<int:pk>/', VersionDetailView.as_view(), name='version_detail'),
 ]
